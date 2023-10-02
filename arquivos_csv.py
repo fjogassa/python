3b9f34d825f940766be0a750d227570df4e0e6cf
Fabio Jun <fabio.jun@elotech.com.br>
2023-10-02 16:52:17 -0300

    Criação do método para criar o arquivo csv

--- arquivos_csv.py
+++ arquivos_csv.py
@@ -4,6 +4,7 @@ class Arquivo:
   nome_arquivo:str = None
   separador:str = None
   decimal_separator:str = None
+  arquivo_data_frame: any = None
   # constructor
   def __init__(self, nome_arquivo = None, separador = None, decimal_separator = None) -> None:
     self.nome_arquivo = nome_arquivo
@@ -11,34 +12,32 @@ class Arquivo:
     self.decimal_separator = decimal_separator
 
   def manipula_arquivo(self):
-    #print(f"Nome do arquivo: {self.nome_arquivo}")
-    print(f"Separador: {self.separador}")
-    print(f"Decimal: {self.decimal_separator}")
-    df = pd.read_csv(self.nome_arquivo, sep = self.separador, decimal = self.decimal_separator)
-    print(df)
+    self.arquivo_data_frame = pd.read_csv(self.nome_arquivo, sep = self.separador, decimal = self.decimal_separator)    
+    print(self.arquivo_data_frame)
     # Filtra os dez primeiros resultados
-    #df.head(10)
-    #df.isnull().sum()
-    """
+    self.arquivo_data_frame.head(10)
+    self.arquivo_data_frame.isnull().sum()
+    
     # Tratamento para converter notas nulas em zero
     notas_nulas = {"Notas": 0,"Media": 0}
-    df.fillna(value = notas_nulas, inplace = True)
+    self.arquivo_data_frame.fillna(value = notas_nulas, inplace = True)
 
     # Removendo com base no filtro Nome = Alice ou Nome = Carlos
-    alunos_que_sairam = df.query('Nome == "Alice" | Nome == "Carlos"').index
-    df.drop(alunos_que_sairam, axis=0, inplace=True)
-    print(f'Sem os alunos que saíram\n {df}')
+    alunos_que_sairam = self.arquivo_data_frame.query('Nome == "Alice" | Nome == "Carlos"').index
+    self.arquivo_data_frame.drop(alunos_que_sairam, axis=0, inplace=True)
+    print(f'Sem os alunos que saíram\n {self.arquivo_data_frame}')    
 
-    aprovados = df['Aprovado'] == True
-    df_aprovados = df[aprovados]
-    print(f'Lista dos Aprovados\n {df_aprovados}')
-    """
-
-# Criando arquivo CSV
-#df_aprovados.to_csv('alunos_aprovados.csv', sep=';')
-#df_aprovados.replace({'Notas':7.0}, 8.0, inplace=True)
-#print(f'Aprovados: {df_aprovados}')
+  # Criando arquivo CSV
+  def cria_arquivo(self, nome_arquivo_destino:str = None) -> None:    
+    aprovados = self.arquivo_data_frame['Aprovado'] == True
+    df_aprovados = self.arquivo_data_frame[aprovados]
+    print(f'Lista dos Aprovados\n {df_aprovados}') 
+    
+    df_aprovados.to_csv(nome_arquivo_destino, sep=';')
+    df_aprovados.replace({'Notas':7.0}, 8.0, inplace=True)
+    print(f'Aprovados: {df_aprovados}')
 
 if __name__ == "__main__":
-  arquivo_csv = Arquivo('alunos.csv', ',', '.')
+  arquivo_csv = Arquivo('alunos.csv', ';', '.')
   arquivo_csv.manipula_arquivo()
+  arquivo_csv.cria_arquivo('alunos_aprovados.csv')
--- imobiliaria.py
+++ imobiliaria.py
@@ -1,15 +1,4 @@
 import arquivos_csv as csv
-import pandas as pd
 
-#arquivo = pd.read_csv("aluguel.csv", sep=";", decimal=".")
-
-
-arquivo = csv.Arquivo()
-
-arquivo.nome_arquivo = "aluguel.csv"
-arquivo.separador = ";"
-arquivo.decimal_separator = "."
-
+arquivo = csv.Arquivo("aluguel.csv", ";", ".")
 arquivo.manipula_arquivo()
-
-print(arquivo)

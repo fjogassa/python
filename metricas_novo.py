67c94171ba4af6e58aa059d8f35841edab8c8151
Fábio Jun <fabio.jun@elotech.com.br>
2023-10-08 19:51:27 -0300

    . Refactor da classe de arquivos .. Foi passado para a classe o método de tratamento de data nula .. Criação do arquivo de saída

--- metricas_novo.py
+++ metricas_novo.py
@@ -1,16 +1,13 @@
+import arquivos_csv as arquivocsv
 import pandas as pd
 import datetime as dt
 
 class LoadMetricas:
   arquivo_inicial = pd.DataFrame()
+  _arquivo = None
+
   def __init__(self):
-    self.arquivo_inicial = pd.read_csv(".\\arquivos\\metricas.csv")
-
-  def criaDataFrameSaida(self, file: pd.DataFrame, campos) -> pd.DataFrame:
-    return self.arquivo_inicial[campos].copy()
-
-  def limpaCamposDataNula(self, campo:str):    
-      self.arquivo_inicial[campo].fillna(value=pd.NaT, inplace=True)      
+    self.arquivo_inicial = _arquivo.leArquivo()
 
   def diferencaEntreDatas(self, coluna1:str, coluna2:str, colunaResultado: str) -> pd.DataFrame:    
     for index in range(len(self.arquivo_inicial)):
@@ -21,14 +18,20 @@ class LoadMetricas:
         else:          
           self.arquivo_inicial.loc[index, colunaResultado]= (self.arquivo_inicial[coluna1][index] - self.arquivo_inicial[coluna2][index]).days          
 
-if __name__ == "__main__":
-  metricas = LoadMetricas()  
-  metricas.arquivo_inicial["Code review"] = pd.to_datetime(metricas.arquivo_inicial["Code review"])    
-  metricas.arquivo_inicial["In Progress"] = pd.to_datetime(metricas.arquivo_inicial["In Progress"])      
-  metricas.limpaCamposDataNula("Code review")
-  metricas.limpaCamposDataNula("In Progress")
-  metricas.diferencaEntreDatas("Code review", "In Progress", "Devtime")    
-  campos_to_copy = ['id', 'To Do', 'In Progress', 'Assignee']
-  df = metricas.criaDataFrameSaida(metricas.arquivo_inicial, campos_to_copy)
-  print(df)
-  metricas.arquivo_inicial.to_csv("novo_metricas.csv", sep=";", decimal=".")  
+if __name__ == "__main__":  
+  _arquivo = arquivocsv.ArquivoCSV(".\\arquivos\\metricas.csv", ",", ".")  
+  metricas = LoadMetricas()
+
+  if (not metricas.arquivo_inicial.empty):
+    metricas.arquivo_inicial["Code review"] = pd.to_datetime(metricas.arquivo_inicial["Code review"])    
+    metricas.arquivo_inicial["In Progress"] = pd.to_datetime(metricas.arquivo_inicial["In Progress"])      
+    metricas.arquivo_inicial["Done"] = pd.to_datetime(metricas.arquivo_inicial["Done"])      
+
+    _arquivo.limpaCamposDataNula(metricas.arquivo_inicial, "Code review")
+    _arquivo.limpaCamposDataNula(metricas.arquivo_inicial, "In Progress")
+    _arquivo.limpaCamposDataNula(metricas.arquivo_inicial, "Done")
+    
+    metricas.diferencaEntreDatas("Code review", "In Progress", "Devtime")    
+    campos_to_copy = ['id', 'To Do', 'In Progress', 'Assignee']
+    df = _arquivo.criaDataFrameSaida(metricas.arquivo_inicial, campos_to_copy)    
+    _arquivo.criaArquivo(df, "novo_metricas",";",".")    

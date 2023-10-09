595fa1d4b08c83266a2cd346c67163bf6d470ff8
Fábio Jun <fabio.jun@elotech.com.br>
2023-10-08 22:19:19 -0300

    Refator de atributos da classe

--- metricas_novo.py
+++ metricas_novo.py
@@ -2,14 +2,16 @@ import arquivos_csv as arquivocsv
 import pandas as pd
 import datetime as dt
 
-class LoadMetricas:
-  arquivo_inicial = pd.DataFrame()
-  _arquivo = None
+class LoadMetricas:  
+  campos_saida = ["id","Backlog","To Do","In Progress","Code review","In Code Review","Waiting Deploy","To Test"
+                  ,"Testing","To Release","Done","Type","Priority","Components","Assignee","Blocked Days","Link Epic","Size"]
 
-  def __init__(self):
-    self.arquivo_inicial = _arquivo.leArquivo()
+  def __init__(self, nomeArquivo, separador, decimalSeparador):
+    self._arquivo = arquivocsv.ArquivoCSV(nomeArquivo, separador, decimalSeparador)
+    self.arquivo_inicial = self._arquivo.leArquivo()
 
-  def diferencaEntreDatas(self, coluna1:str, coluna2:str, colunaResultado: str) -> pd.DataFrame:    
+  # Método que calcula a diferença em dias de duas datas (coluna1 e coluna2) e atribui-se na colunaResultado
+  def diferencaEntreDatas(self, coluna1:str, coluna2:str, colunaResultado: str) -> pd.DataFrame:        
     for index in range(len(self.arquivo_inicial)):
       self.arquivo_inicial.loc[index, colunaResultado] = int(0)
       if (not pd.isnull(self.arquivo_inicial[coluna1][index])) and (not pd.isnull(self.arquivo_inicial[coluna2][index])):
@@ -19,19 +21,18 @@ class LoadMetricas:
           self.arquivo_inicial.loc[index, colunaResultado]= (self.arquivo_inicial[coluna1][index] - self.arquivo_inicial[coluna2][index]).days          
 
 if __name__ == "__main__":  
-  _arquivo = arquivocsv.ArquivoCSV(".\\arquivos\\metricas.csv", ",", ".")  
-  metricas = LoadMetricas()
+  metricas = LoadMetricas(".\\arquivos\\metricas.csv", ",", ".")
 
   if (not metricas.arquivo_inicial.empty):
     metricas.arquivo_inicial["Code review"] = pd.to_datetime(metricas.arquivo_inicial["Code review"])    
     metricas.arquivo_inicial["In Progress"] = pd.to_datetime(metricas.arquivo_inicial["In Progress"])      
     metricas.arquivo_inicial["Done"] = pd.to_datetime(metricas.arquivo_inicial["Done"])      
 
-    _arquivo.limpaCamposDataNula(metricas.arquivo_inicial, "Code review")
-    _arquivo.limpaCamposDataNula(metricas.arquivo_inicial, "In Progress")
-    _arquivo.limpaCamposDataNula(metricas.arquivo_inicial, "Done")
+    metricas._arquivo.limpaCamposDataNula(metricas.arquivo_inicial, "Code review")
+    metricas._arquivo.limpaCamposDataNula(metricas.arquivo_inicial, "In Progress")
+    metricas._arquivo.limpaCamposDataNula(metricas.arquivo_inicial, "Done")
     
     metricas.diferencaEntreDatas("Code review", "In Progress", "Devtime")    
-    campos_to_copy = ['id', 'To Do', 'In Progress', 'Assignee']
-    df = _arquivo.criaDataFrameSaida(metricas.arquivo_inicial, campos_to_copy)    
-    _arquivo.criaArquivo(df, "novo_metricas",";",".")    
+    campos_to_copy = ['id', 'To Do', 'In Progress', 'Assignee']    
+    
+    metricas._arquivo.criaArquivo(metricas._arquivo.criaDataFrameSaida(metricas.arquivo_inicial, campos_to_copy), "novo_metricas",";",".")    
